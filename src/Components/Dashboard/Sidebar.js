40295fe11710a345d0a97179f0deed5989775bdb
Mason Reichbauer <mreichba@mail.usf.edu>
2020-02-13 16:13:07 -0500

    fixed width of slidebar

--- src/Components/Dashboard/Sidebar.js
+++ src/Components/Dashboard/Sidebar.js
@@ -3,7 +3,7 @@ import { slide as Menu } from 'react-burger-menu';
 
 export default props => {
   return (
-    <Menu width={'30%'} right>
+    <Menu width={'20%'} right>
       <a className="menu-item" href="/">
         Home
       </a>
