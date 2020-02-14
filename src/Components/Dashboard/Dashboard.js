6e46d32dd0224599e10ac69b66b9176eff85ccd0
Mason Reichbauer <mgreichba@gmail.com>
2020-02-14 12:19:25 -0500

    added new slide menu

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import SideBar from './Sidebar';
 import Context from '../Context/Context';
 import { Link } from 'react-router-dom';
 import './Dashboard.css';
-
-// note still have work to do, working on implementing the side bar
-// and main questionnaire area, used some code from last capstone as starter
-// export default function Dashboard() {
-//   return (
-//     <div id="Dashboard">
-//       <SideBar />
-//       <div id="page-wrap">
-//         <h1>Sidebar</h1>
-//         <h2>Placeholder</h2>
-//       </div>
-//     </div>
-//   );
-// }
+import SlideMenu from '../Slide-Menu/Slide-Menu'
 
 class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+  }
   static contextType = Context
   static defaultProps = {
     language: {
@@ -42,11 +34,11 @@ class Dashboard extends React.Component {
     console.log(this.context)
     return (
       <div className="dashboardContainer">
-        <SideBar className='sideBar' />
-        <div id="page-wrap">
-          <h1>AppDividend</h1>
-          <h2>Check out our tutorials the side menubar</h2>
+        <h1>Dashboard</h1>
+        <div>
+          <SlideMenu open={this.state.open} />
         </div>
+
         {/* <h2>Welcome back, {this.context.currentUser}!
           </h2>
 
