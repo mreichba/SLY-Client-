d1c9a19930ba8d28e2b987b98bd2f10d9132c2b5
AnthonyTB <anthonylol@protonmail.com>
2020-03-07 21:26:20 -0800

    when jwt expires it now auto directs user to login route and also removed the slide menu and made sure each route and component had a smoke test

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
 export default {
-  API_ENDPOINT: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api'
+  API_ENDPOINT:
+    process.env.REACT_APP_API_BASE_URL ||
+    'https://infinite-journey-17550.herokuapp.com/api' ||
+    'http://localhost:8000/api'
 };
